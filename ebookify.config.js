e056fcaaed72745e93e712cf065336801c14f20d
John Stephens <john@cyclexo.com>
2017-10-03 20:31:58 -0400

    Add sample config file to repository

---
+++ ebookify.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+
+  // Set path to calibre's ebook-convert script
+  'ebook_convert_path': '/usr/bin/ebook-convert',
+
+  // Set URL to e-book cover image
+  'ebook_cover_url': ''
+
+};
